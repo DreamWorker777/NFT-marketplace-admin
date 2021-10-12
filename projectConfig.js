2d66273cc23e2372094f7548faa1a61df56e91f3
dreamworker <devmetaverse34@gmail.com>
2021-10-12 23:16:02 +0300

    apiURL change

--- projectConfig.js
+++ projectConfig.js
@@ -1,2 +1,6 @@
 // Backend server api URL
-export const $apiURL = 'http://127.0.0.1:3000/api/'
+// export const $apiURL = 'http://127.0.0.1:3000/api/'
+
+
+//production
+export const $apiURL = 'https://truhelix.com/node/api/'
